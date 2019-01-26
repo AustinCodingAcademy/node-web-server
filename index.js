552ee58777a4a06a59dd3155d773756b93f26445
Darwin Risser <riser.darwin@gmail.com>
2019-01-26 16:02:40 -0600

    users and products working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 let users = require("./state").users;
+let products = require("./state").products;
 let server = http.createServer(messageReceived);
 server.listen(8080);
 
@@ -7,13 +8,23 @@ function messageReceived(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   if(req.method === "GET" && req.url.indexOf("/users/") > -1){
     let id = req.url.split("/");
-    let user = users.find(p=>p["_id"] == id[2]);
+    let user = users.find(p=>p["id"] == id[2]);
     let userJSON = JSON.stringify(user);
     res.write(userJSON);
+  }
+  if(req.method === "GET" && req.url.indexOf("/products/") > -1){
+    let id = req.url.split("/");
+    let product = products.find(p=>p["id"] == id[2]);
+    let productJSON = JSON.stringify(product);
+    res.write(productJSON);
   }
   else if(req.method === "GET" && req.url === "/users"){
     let usersJSON = JSON.stringify(users);
     res.write(usersJSON);
+  }
+  else if(req.method === "GET" && req.url === "/products"){
+    let productsJSON = JSON.stringify(products);
+    res.write(productsJSON);
   }
   else if(req.method === "POST" && req.url === "/users"){
     let body = [];
@@ -23,6 +34,16 @@ function messageReceived(req, res) {
       body = Buffer.concat(body).toString();
       let user = JSON.parse(body);
       users.push(user);
+    });
+   }
+   else if(req.method === "POST" && req.url === "/products"){
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      let product = JSON.parse(body);
+      products.push(product);
     });
    }
   else{
