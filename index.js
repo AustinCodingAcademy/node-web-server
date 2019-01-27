e91799bbf7afa03677ec7ab5d0c165c28d42a062
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-27 14:36:40 -0600

    can't get post to work still

--- index.js
+++ index.js
@@ -5,28 +5,46 @@ let products = require("./state").products;
 let server = http.createServer(messageReceived);
 server.listen(8080);
 
+const postChucks = (req, res,urlselected) => {
+    let body = [];
+    req.on('data', (chunk) => {
+    body.push(chunk);
+    }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    let user = JSON.parse(body);
+    urlselected.push(user);
+    });
+}
+
 function messageReceived(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   const firstTerm = req.url.split("/")[1];
   const secondTerm = req.url.split("/")[2];
+
+  if(firstTerm=="favicon.ico"){
+    res.end();
+    return
+  }
+
   if(req.method === "GET" && secondTerm){
     let urlselected = requestSelector(firstTerm)
     let user = urlselected.find(p=>p["id"] == secondTerm);
-    let usersJSON = JSON.stringify(user);
-    res.write(usersJSON);
+    if(!user){
+      res.write("Not Found");
+    }else{
+      let usersJSON = JSON.stringify(user);
+      res.write(usersJSON);
+    }
+    
   }else if(req.method === "GET"){
     let urlselected = requestSelector(firstTerm)
     let usersJSON = JSON.stringify(urlselected);
     res.write(usersJSON);
-  }else if(req.method === "POST" && req.url === "/users"){
-   let body = [];
-   req.on('data', (chunk) => {
-     body.push(chunk);
-   }).on('end', () => {
-     body = Buffer.concat(body).toString();
-     let user = JSON.parse(body);
-     users.push(user);
-   });
+  }else if(req.method === "POST" && !secondTerm){
+    let urlselected = requestSelector(firstTerm)
+    postChucks(req, res,urlselected);
+    res.write(JSON.stringify(urlselected));
+
   }
 
   else{
