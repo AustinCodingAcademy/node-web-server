5d9d3aca9e3ab84c15b4d5aee2fecb763e10bb1d
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-29 16:54:49 -0600

    fixed code back to id rather than index

--- index.js
+++ index.js
@@ -6,14 +6,34 @@ let server = http.createServer(messageReceived);
 server.listen(8080);
 
 const postChucks = (req, res,urlselected) => {
-    let body = [];
-    req.on('data', (chunk) => {
-    body.push(chunk);
-    }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    let user = JSON.parse(body);
-    urlselected.push(user);
-    });
+  let body = [];
+  req.on('data', (chunk) => {
+  body.push(chunk);
+  }).on('end', () => {
+  body = Buffer.concat(body).toString();
+  let user = JSON.parse(body);
+  user.id = urlselected[urlselected.length-1].id+1
+  res.end(JSON.stringify(user))
+  urlselected.push(user);
+  });
+}
+
+const putChucks = (req, res,urlselected,slicePosition) => {
+  let body = [];
+  req.on('data', (chunk) => {
+  body.push(chunk);
+  }).on('end', () => {
+  body = Buffer.concat(body).toString();
+  let user = JSON.parse(body);
+  user.id = urlselected[slicePosition].id
+  res.end(JSON.stringify(user))
+  urlselected[slicePosition]=(user);
+  });
+}
+
+const deleteChucks = (req, res,urlselected,slicePosition) => {
+  res.end("Deleted")
+  urlselected.splice(slicePosition,1)
 }
 
 function messageReceived(req, res) {
@@ -28,29 +48,51 @@ function messageReceived(req, res) {
 
   if(req.method === "GET" && secondTerm){
     let urlselected = requestSelector(firstTerm)
-    let user = urlselected.find(p=>p["id"] == secondTerm);
+    // let user = urlselected.find(p=>p["id"] == secondTerm);
+    let user = null;
+    if(secondTerm){
+      // keeping this if statement in case path changes to what I think
+      // the assigment is asking
+      user = urlselected.find(p=>p["id"] == secondTerm);
+    }else{
+      user = urlselected[Number(secondTerm[1])-1];
+    }
     if(!user){
-      res.write("Not Found");
+      res.end("Not Found");
     }else{
       let usersJSON = JSON.stringify(user);
-      res.write(usersJSON);
+      res.end(usersJSON);
     }
     
   }else if(req.method === "GET"){
     let urlselected = requestSelector(firstTerm)
     let usersJSON = JSON.stringify(urlselected);
-    res.write(usersJSON);
+    res.end(usersJSON);
   }else if(req.method === "POST" && !secondTerm){
     let urlselected = requestSelector(firstTerm)
     postChucks(req, res,urlselected);
-    res.write(JSON.stringify(urlselected));
+    // takes in a request, a response and an array to push the chunck to
+  }else if(req.method === "PUT" && secondTerm){
+    let urlselected = requestSelector(firstTerm)
+    const slicePosition = urlselected.findIndex( (item,index) => {
+      return item.id == secondTerm;
+    })
+    putChucks(req, res,urlselected,Number(slicePosition))
+
+  }else if(req.method === "DELETE" && secondTerm){
+    let urlselected = requestSelector(firstTerm)
+    const slicePosition = urlselected.findIndex( (item,index) => {
+      return item.id == secondTerm;
+    })
+
+    deleteChucks(req, res,urlselected,Number(slicePosition))
 
   }
 
   else{
-   res.write("Not Found");
+   res.end("Not Found");
   }
-  res.end();
+
 }
 
 
