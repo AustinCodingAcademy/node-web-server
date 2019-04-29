2abeb061ced6f7d79525d7a4388e3fcada8de0b9
Collin Betori <cbetori@gmail.com>
2019-04-28 23:23:46 -0500

    updated server

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ let users = require('./state').users
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     if(req.method === "GET" && req.url.indexOf("/products/") > -1){
-        let productsJSON = splitPath(req.url)
+        let productsJSON = splitPath(req.url).productsJSON
         res.write(productsJSON);
     }
     else if(req.method === "POST" && req.url.indexOf("/products/") > -1){
+        let productsJSON = splitPath(req.url)
         res.write("you wanted to make a product")
     }
     else if(req.method === "PUT" && req.url.indexOf("/products/") > -1){
-    res.write("you wanted to update a product")
+        let productName = splitPath(req.url).productName
+        res.write("you wanted to update product "+productName)
     }
     else if(req.method === "DELETE" && req.url.indexOf("/products/") > -1){
-    res.write("you wanted to delete a product")
+        let productName = splitPath(req.url).productName
+        res.write("you wanted to delete product "+productName)
     }
     else{
     res.write("Not Found");
@@ -26,7 +29,6 @@ http.createServer(function (req, res) {
 let productsById =(id)=>{
     let array = []
     for(let i = 0; i<products.length; i++){
-        console.log(products[i])
         if(products[i].id === id){
             array.push(products[i])
         }
@@ -37,10 +39,13 @@ let productsById =(id)=>{
 let splitPath=(string)=>{
     let id = string.split("/"); 
     id = parseFloat(id[2])
-    //let product = products.filter=(product)=>{return product.id === id[2]}
-    //let productsJSON = JSON.stringify(product(products));
     let productsJSON = JSON.stringify(productsById(id))
-    return productsJSON
+    let productName = JSON.stringify(productsById(id)[0].name)
+    return {productsJSON, productName}
+}
+
+let handleDelete=()=>{
+
 }
 //this is homework make this work for users and products
 
