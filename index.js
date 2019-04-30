12f7fa6f8178ffa12aa2911846f47ca0fc43d6d5
Nicolai A <nantonov@utexas.edu>
2019-04-29 21:00:03 -0500

    fixed PUT and POST methods, allowing them to return the new objects to clients

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ let server = http.createServer(function (req, res) {
     if (req.method === "GET" && req.url === "/users") {
         let usersJSON = JSON.stringify(users)
         res.write(usersJSON)
+        res.end();
     }
     if (req.method === "GET" && req.url.indexOf("/users/") > -1) {
         let id = req.url.split("/")
         let user  = users.find(p=>p["_id"] == id[2]);
         let userJSON = JSON.stringify(user)
         res.write(userJSON)
+        res.end();
     }
     if (req.method === "POST" && req.url === "/users") {
         let body = [];
@@ -27,7 +29,8 @@ let server = http.createServer(function (req, res) {
             users.push(user);
             user = JSON.stringify(user)
             res.write(user)
-        })
+            res.end();
+        })        
     }
     if (req.method === "PUT" && req.url.indexOf("/users/") > -1) {
         let body = [];
@@ -38,7 +41,10 @@ let server = http.createServer(function (req, res) {
             body = Buffer.concat(body).toString();
             let user = JSON.parse(body);
             let index = users.indexOf(users.find(p=>p["_id"] == id[2]))
+            user._id = id[2]
             users[index] = user
+            res.write(JSON.stringify(user))
+            res.end();
         });
     }
     if (req.method === "DELETE" && req.url.indexOf("/users/") > -1) {
@@ -48,17 +54,19 @@ let server = http.createServer(function (req, res) {
         //let index = users.indexOf(user)
         //users.splice(index, 1)
         res.write("deleted")
-        };
-
+        res.end();
+    }
     if (req.method === "GET" && req.url === "/products") {
         let productsJSON = JSON.stringify(products)
         res.write(productsJSON)
+        res.end();
     }
     if (req.method === "GET" && req.url.indexOf("/products/") > -1) {
         let id = req.url.split("/")
         let product  = products.find(p=>p["_id"] == id[2]);
         let productJSON = JSON.stringify(product)
         res.write(productJSON)
+        res.end();
     }
     if (req.method === "POST" && req.url === "/products") {
         let body = [];
@@ -70,7 +78,10 @@ let server = http.createServer(function (req, res) {
             let lastIndex = products.length - 1
             product._id = products[lastIndex]._id + 1
             products.push(product);
-        })
+            product = JSON.stringify(product)
+            res.write(product)
+            res.end();
+        });
     }
     if (req.method === "PUT" && req.url.indexOf("/products/") > -1) {
         let body = [];
@@ -81,7 +92,10 @@ let server = http.createServer(function (req, res) {
             body = Buffer.concat(body).toString();
             let product = JSON.parse(body);
             let index = products.indexOf(products.find(p=>p["_id"] == id[2]))
+            product._id = id[2]
             products[index] = product
+            res.write(JSON.stringify(product))
+            res.end();
         });
     }
     if (req.method === "DELETE" && req.url.indexOf("/products/") > -1) {
@@ -91,9 +105,9 @@ let server = http.createServer(function (req, res) {
         //let index = products.indexOf(product)
         //products.splice(index, 1)
         res.write("deleted")
+        res.end();
         };        
 
-res.end();
 });
 
 server.listen(8080)
