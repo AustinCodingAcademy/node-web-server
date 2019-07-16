bb735b04e2f36a86a929a851667f00bc0c4471e4
Joseph William Hauger <joseph>
2019-07-16 16:29:43 -0500

    Part 1 mostly working

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+var http = require('http');
+let state = require('./state');
+let users = state.users;
+let products = state.products;
 
+
+let server = http.createServer(messageReceived);
+server.listen(8080);
+
+function messageReceived(req, res) {
+ res.writeHead(200, {'Content-Type': 'text/plain'});
+ if(req.method === "GET" && req.url === "/users"){
+    let stateJSON = JSON.stringify(state);
+     res.write(stateJSON);
+ }
+ else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
+    let id = req.url.split('/');
+    let user = users.find(u=>u["_id"] === Number(id[2]))
+    let userJSON = JSON.stringify(user);
+     res.write(userJSON); 
+ }
+ else if(req.method === "POST" && req.url ==="/users"){
+    let body = [];
+    req.on('data', (chunk)=>{
+        body.push(chunk);
+    }).on('end', ()=>{
+        body = Buffer.concat(body).toString();
+     let user = JSON.parse(body);
+     users.push(user);
+     res.write(user);
+     res.end();
+    });
+ } 
+ else if(req.method === "PUT" && req.url.indexOf("/users/")> -1){
+    let body = [];
+    let id = req.url.split('/');
+    let update = users.find(u=>u["_id"] === Number(id[2]))
+    req.on('data', (chunk)=>{
+        body.push(chunk);
+    }).on('end', ()=>{
+        body = Buffer.concat(body).toString();
+        body = JSON.parse(body);
+        user.name = body.name;
+        res.write(JSON.stringify(user))
+        res.end();
+    });
+ }
+ else if(req.method === "DELETE" && req.url.indexOf("/user/")>-1){
+    let id = req.url.split('/');
+    let update = users.find(u=>u["_id"] === Number(id[2]))
+    users.splice(update, 1);
+ }
+ else{
+     res.write("Not Found");
+ }
+ res.end();
+} 
