17707331b3d56db606623ecf2f21410f1905e637
Joseph William Hauger <joseph>
2019-07-18 12:27:21 -0500

    delete not working

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function messageReceived(req, res) {
  }
  else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
     let id = req.url.split('/');
-    let user = users.find(u=>u["_id"] === Number(id[2]))
+    let user = users.find(u=>u["id"] === Number(id[2]))
     let userJSON = JSON.stringify(user);
      res.write(userJSON); 
  }
@@ -26,28 +26,29 @@ function messageReceived(req, res) {
     }).on('end', ()=>{
      body = Buffer.concat(body).toString();
      let user = JSON.parse(body);
-     user.id = users.length+1;
      users.push(user);
+     user.id = users.length+1;
     });
  } 
  else if(req.method === "PUT" && req.url.indexOf("/users/")> -1){
     let body = [];
     let id = req.url.split('/');
-    let update = users.find(u=>u["_id"] === Number(id[2]))
+    let update = users.find(u=>u["id"] === Number(id[2]));
     req.on('data', (chunk)=>{
         body.push(chunk);
     }).on('end', ()=>{
         body = Buffer.concat(body).toString();
         body = JSON.parse(body);
-        user.name = body.name;
-        res.write(JSON.stringify(user))
-        res.end();
+        update.name = body.name;
     });
  }
  else if(req.method === "DELETE" && req.url.indexOf("/user/")>-1){
     let id = req.url.split('/');
-    let update = users.find(u=>u["_id"] === Number(id[2]))
-    users.splice(update, 1);
+    let dump = users.find(u=>u["id"] === Number(id[2]));
+    //dump.isActive = false;
+    users.splice(dump, 1);
+    res.write("deleted");
+    
  }
  else{
      res.write("Not Found");
